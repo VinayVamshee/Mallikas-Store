dbe4cd90fff6d80d0bcfe87be38971a5ec37a622
Vinay Vamshee <vinayvamshee@Vinays-MacBook-Air.local>
2025-06-05 18:41:40 +0530

    Adding Google Analytics

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
+
 import Home from './Components/Home';
 import Navigation from './Components/Navigation';
 import './Components/style.css';
@@ -9,18 +12,34 @@ import Product from './Components/Product';
 import User from './Components/User';
 import Admin from './Components/Admin';
 
+// ✅ Initialize Google Analytics
+const TRACKING_ID = 'G-ZKQXRPHC86';
+ReactGA.initialize(TRACKING_ID);
+
+// ✅ Track page views on route change
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: 'pageview', page: location.pathname });
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <AnalyticsTracker />
       <div className="App">
         <Navigation />
         <Routes>
           <Route path="/" exact element={<Home />} />
-          <Route path='/Apparels' element={<Apparels />} />
-          <Route path='/Accessories' element={<Accessories />} />
-          <Route path='/Product' element={<Product />} />
-          <Route path='/User' element={<User />} />
-          <Route path='/Admin' element={<Admin />} />
+          <Route path="/Apparels" element={<Apparels />} />
+          <Route path="/Accessories" element={<Accessories />} />
+          <Route path="/Product" element={<Product />} />
+          <Route path="/User" element={<User />} />
+          <Route path="/Admin" element={<Admin />} />
         </Routes>
         <Footer />
       </div>
